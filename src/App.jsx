b5bff468639a2b5ee25d498c7e5028a16285ce44
rajsharma-sudo <rajparashar931@gmail.com>
2024-04-04 11:38:15 +0530

    hero component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
