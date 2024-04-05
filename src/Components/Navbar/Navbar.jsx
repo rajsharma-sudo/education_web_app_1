22fc1eacb3a562c739d37714e1086a61329386c2
rajsharma-sudo <rajparashar931@gmail.com>
2024-04-05 12:27:04 +0530

    here we have completed navbar scrolling feature

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import LOGO from '../../assets/logo.png'
-const Navbar = () => {
-  return (
-    <nav className='container dark-nav'>
 
-    <img src={LOGO} alt="" className='logo' />
-     <ul>
-      <li>Home</li>
-      <li>Program</li>
-      <li>About us</li>
-      <li>Campus</li>
-      <li>Testimonials</li>
-      <li><button className='btn'>Contact us</button></li>
-     </ul>
+
+const Navbar = () => {
+  // here we write navbar scrolling feature logic
+  const [sticky, setSticky] = useState(false)
+ 
+  useEffect(()=>{
+     window.addEventListener('scroll',()=>{
+      window.scrollY > 50 ? setSticky(true) : setSticky(false)
+     })
+  },[])
+
+  return (
+
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+
+      <img src={LOGO} alt="" className='logo' />
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li><button className='btn'>Contact us</button></li>
+      </ul>
 
     </nav>
+
   )
+
 }
 
 export default Navbar
