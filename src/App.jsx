d66cf7aaea0044155763fdbf3fab75aea8814b02
rajsharma-sudo <rajparashar931@gmail.com>
2024-04-05 12:49:19 +0530

    now we have completed the campus section

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
 import Title from "./Components/Title/Title";
 import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
       <div className="container">
-        <Title subtitle='Our PROGRAM' title='What We Offer'/>
-      <Programs/>
+        <Title subtitle='Our PROGRAM' title='What We Offer' />
+        <Programs />
+        <About />
+        <Title subtitle='Gallery' title='Campus Photos' />
+        <Campus/>
       </div>
 
-      <About/>
 
 
 
